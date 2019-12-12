202ab59ded824cb174577d22070b2f2498850bc6
Kevit Technologies <pratik.mukherjee@kevit.io>
2019-12-13 00:52:46 +0530

    second commit

--- testproject/testapp/views.py
+++ testproject/testapp/views.py
@@ -13,3 +13,19 @@ def home(request):
         'longitude': geodata['longitude'],
         'api_key': 'AIzaSyC1UpCQp9zHokhNOBK07AvZTiO09icwD8I'  # Don't do this! This is just an example. Secure your keys properly.
     })
+
+
+ef github(request):
+    search_result = {}
+    if 'username' in request.GET:
+        username = request.GET['username']
+        url = 'https://api.github.com/users/%s' % username
+        response = requests.get(url)
+        search_was_successful = (response.status_code == 200)  # 200 = SUCCESS
+        search_result = response.json()
+        search_result['success'] = search_was_successful
+        search_result['rate'] = {
+            'limit': response.headers['X-RateLimit-Limit'],
+            'remaining': response.headers['X-RateLimit-Remaining'],
+        }
+    return render(request, 'core/github.html', {'search_result': search_result})
